c1c7a6392a9cbe9247591708ac04bd260d1c9528
hugi-codes <mindthehug@googlemail.com>
2025-01-24 14:09:37 +0000

    changed order of input cols

--- deployment/local-deployment/score_passenger.py
+++ deployment/local-deployment/score_passenger.py
@@ -6,32 +6,32 @@ url = "http://127.0.0.1:9696/predict"
 # Create a sample JSON payload with input data
 # Updated to include categorical columns
 
+print("test")
+
 airline_passenger_data = {
-    "Gender": "Male",  # Gender doesn't impact prediction but included for consistency.
-    "Customer Type": "Loyal Customer",  # Loyal customer is more likely to have a perfect experience.
-    "Type of Travel": "Business travel",  # Business travel generally correlates with better experiences.
-    "Class": "Business",  # Business class passengers typically experience better service.
-    "Age": 40,
-    "Flight Distance": 8000,  # Long-haul flight, implying a more comfortable and premium service.
-    "Inflight wifi service": 5,  # Excellent wifi service (perfect experience).
-    "Departure/Arrival time convenient": 5,  # Extremely convenient departure and arrival times.
-    "Ease of Online booking": 5,  # Very easy and smooth online booking process.
-    "Gate location": 5,  # Very convenient gate location.
-    "Food and drink": 5,  # Excellent food and drink service.
-    "Online boarding": 5,  # Very smooth and easy online boarding.
-    "Seat comfort": 5,  # Very comfortable seating.
-    "Inflight entertainment": 5,  # Excellent inflight entertainment options.
-    "On-board service": 5,  # Top-tier on-board service.
-    "Leg room service": 5,  # Plenty of leg room.
-    "Baggage handling": 5,  # Fast and efficient baggage handling.
-    "Checkin service": 5,  # Very smooth and fast check-in process.
-    "Inflight service": 5,  # Excellent inflight service.
-    "Cleanliness": 5,  # Immaculately clean environment.
-    "Departure Delay in Minutes": 0,  # No delays, ideal scenario.
-    "Arrival Delay in Minutes": 0  # No delays, ideal scenario.
+    "Gender": "Male",
+    "Customer Type": "Loyal Customer",  # You can fill this in with an appropriate value
+    "Age": 32,
+    "Type of Travel": "Business travel",
+    "Class": "Business",
+    "Flight Distance": 2000,
+    "Inflight wifi service": 5,
+    "Departure/Arrival time convenient": 5,
+    "Ease of Online booking": 5,
+    "Gate location": 5,
+    "Food and drink": 5,
+    "Online boarding": 5,
+    "Seat comfort": 5,
+    "Inflight entertainment": 5,
+    "On-board service": 5,
+    "Leg room service": 5,
+    "Baggage handling": 5,
+    "Checkin service": 5,
+    "Inflight service": 5,
+    "Cleanliness": 5,
+    "Departure Delay in Minutes": 0,
+    "Arrival Delay in Minutes": 0.0
 }
-
-
 
 
 
